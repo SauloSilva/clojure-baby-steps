29588f7591200360bfa72e4fb8155ccf664982d7
Saulo Santiago <saulodasilvasantiago@gmail.com>
2016-05-11 20:07:17 -0300

    Adding execute core to async-tea-party on command line

--- async-tea-party/src/async_tea_party/core.clj
+++ async-tea-party/src/async_tea_party/core.clj
@@ -1,8 +1,10 @@
 (ns async-tea-party.core
+  (:gen-class)
   (:require [clojure.core.async :as async]))
 
 (def google-tea-service-chan (async/chan 10))
 (def yahoo-tea-service-chan (async/chan 10))
+(def result-chan (async/chan 10))
 
 (defn random-add []
   (reduce + (conj [] (repeat (rand-int 10000) 1))))
@@ -17,13 +19,15 @@
     (random-add)
     (async/>! yahoo-tea-service-chan "team compliments of yahoo")))
 
-
 (defn request-tea []
   (request-google-tea-service)
   (request-yahoo-tea-service)
   (async/go (let [[v] (async/alts!
                         [google-tea-service-chan
                          yahoo-tea-service-chan])]
-              (println v))))
+              (async/>! result-chan v))))
 
-(request-tea)
+(defn -main [& args]
+  (println "Requesting tea")
+  (request-tea)
+  (println (async/<!! result-chan)))
